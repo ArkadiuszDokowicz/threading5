ee6aba767d42788e20e266c1fd366043f6b16ea4
ArkadioMorales9 <arkdokowicz@gmail.com>
2019-05-06 21:59:59 +0200

    pom for javaFX prepared

--- src/main/java/threading5/Main.java
+++ src/main/java/threading5/Main.java
@@ -1,9 +1,23 @@
 package threading5;
 
-public class Main{
-    public static void main(String [] args)
-    {
-        System.out.println("hello world");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FXApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/threading4.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
